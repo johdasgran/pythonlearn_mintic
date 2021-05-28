560431ff2dc3615a0bb76169b8127cc8fbd583c2
johang <dampig14@gmail.com>
2021-05-27 21:02:39 -0500

    Detectar OS y ejecutar comandos de terminal en python

--- reto1.py
+++ reto1.py
@@ -1,5 +1,10 @@
 ## Actividad Reto de la semana: Elabora un programa de inicio de sesión con validación de captcha de seguridad. (Reto 1)
 
+# platform: detecta el os desde el que se ejecuta el .py
+# os: permite ejecutar comandos de consola (Linux / Windows / Mac)
+import platform
+import os
+
 ## RF01 - El programa dispone de un mensaje de bienvenida al sistema
 ##        previo a la solicitud de las credenciales de acceso.
 
@@ -25,10 +30,22 @@ else:
         Captcha_Math = (6*2)-5-5
         Captcha_Final = (Captcha + Captcha_Math)
         Capcha_Validate = int(input("Complete la suma 625 + 2: "))
-        
+
         # RF04: - : El programa confirma el ingreso al sistema con un mensaje de
         #            éxito en el inicio de sesión.
         if Captcha_Final == Capcha_Validate:
+
+            ## Clear screent
+            #8 sistema: variable que guarda el nombre del sistema operativo como un string 
+            sistema = platform.system()
+            # Show the os
+            # print("Estamos en {}".format(sistema))
+
+            if sistema == "Linux" or sistema == "Darwin":
+                os.system('clear')
+            elif sistema == "Windows":
+                os.system('cls')
+
             print ("Sesión iniciada")
         else: 
            print ("Error") 
